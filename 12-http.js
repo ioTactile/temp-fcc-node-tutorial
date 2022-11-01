39d3c7c92614afb39f2eccccad250344fcee0d95
jordan <jbs.io@protonmail.com>
2022-11-01 13:42:12 +0100

    first commit

---
+++ 12-http.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  console.log(req)
+  if (req.url === '/') {
+    res.end('Welcome to our home page')
+  }
+  if (req.url === '/about') {
+    res.end('here is our short History')
+  }
+  res.end(`
+  <h1>Oops!</h1>
+  <p>We can't seem to find the page you are looking for</p>
+  <a href="/">back home</a>
+  `)
+})
+
+server.listen(5000)
